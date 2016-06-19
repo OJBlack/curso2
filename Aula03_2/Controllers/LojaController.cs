a92dec53c8fd0a6a926ab9346a536abe5f05ab59
Osmar <Osmar@BLACKSTORMPC>
2016-06-19 12:55:54 -0300

    Segundo Commit

---
+++ Aula03_2/Controllers/LojaController.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Web.Mvc;
+
+namespace Aula03_2.Controllers
+{
+    public class LojaController : Controller
+    {
+        // GET: Loja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
